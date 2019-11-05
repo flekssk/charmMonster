6e23781f888a4dcedbb0f21abc6205c130ffaa1d
Aleksey Osipov <osipov@action-media.ru>
2019-11-06 00:37:24 +0300

    add tabs to product complectation

--- backend/resources/views/catalog/product_complectation_tab.blade.php
+++ backend/resources/views/catalog/product_complectation_tab.blade.php
@@ -16,13 +16,39 @@
         data-store-complection-selected-product-url="{{ action('Catalog\ProductController@storeSelectedComplectionProduct') }}"
 >
     @if($product->complectation->count() > 0)
-        <?php /** @var \App\Models\Product\ProductComplectation $complectation */ ?>
-        @foreach($product->complectation->items() as $complectation)
-            <div>
-                <div class="addComplectionCategoryContainer">
+        <ul class="nav nav-tabs">
+            <?php
+            $active = true;
+            /**
+             * @var \App\Models\Product\ProductComplectation $complectation
+             */
+            ?>
+            @foreach($product->complectation->items() as $complectation)
+                <li class="{{ $active ? 'active' : '' }}">
+                    <a href="#tab-complectation-{{ $complectation->id }}" data-toggle="tab">
+                        {{ $complectation->name }}
+                    </a>
+                </li>
+                <?php
+                $active = false;
+                ?>
+            @endforeach
+        </ul>
+
+        <div class="tab-content">
+            <?php
+            $active = true;
+            /**
+             * @var \App\Models\Product\ProductComplectation $complectation
+             */
+            ?>
+            @foreach($product->complectation->items() as $complectation)
+                <div
+                        class="addComplectionCategoryContainer tab-pane {{ $active ? 'active' : '' }}"
+                        id="tab-complectation-{{ $complectation->id }}"
+                >
                     <div class="complectationTitle">
                         <h4>
-                            {{ $complectation->name }}
                             <span
                                     class="destroyComplection"
                                     data-destroy-url="
@@ -34,22 +60,21 @@
                                                 ]
                                             )
                                         }}
-                                    "
+                                            "
                             >
                             (удалить)
                         </span>
                         </h4>
                     </div>
-                    <blockquote>
-                        <div>
-                            <?php /** @var \App\Models\Product\ProductComplectationCategory $complectationCategory */ ?>
-                            @foreach($complectation->categories->items() as $complectationCategory)
-                                <div class="complectionCategory">
-                                    <div class="complectationTitle">
-                                        {{ $complectationCategory->category->description->name }}
-                                        <span
-                                                class="destroyComplectionCategory"
-                                                data-destroy-url="{{
+                    <div>
+                        <?php /** @var \App\Models\Product\ProductComplectationCategory $complectationCategory */ ?>
+                        @foreach($complectation->categories->items() as $complectationCategory)
+                            <div class="complectionCategory">
+                                <div class="complectationTitle">
+                                    {{ $complectationCategory->category->description->name }}
+                                    <span
+                                            class="destroyComplectionCategory"
+                                            data-destroy-url="{{
                                                 action(
                                                     'Catalog\ProductController@destroyComplectionCategory',
                                                     [
@@ -57,57 +82,60 @@
                                                     ]
                                                 )
                                             }}"
-                                        >
+                                    >
                                         (удалить)
                                     </span>
-                                        <span class="help-block">При выборе товара он будет выбран по умолчанию при покеупке товара</span>
-                                    </div>
-                                    @forelse($complectationCategory->products->items() as $product)
-                                        <div
-                                                class="complectationProduct {{ $complectationCategory->getSelectedClass($product->product_id)  }}"
-                                                data-complection-id="{{ $complectation->id }}"
-                                                data-category-id="{{ $complectationCategory->category_id }}"
-                                                data-product-id="{{ $product->product_id }}"
-                                        >
-                                            <img src="{{ getImagePath($product->image) }}" alt="">
-                                        </div>
-                                    @empty
-                                        <span class="help-block">Товарова нет!</span>
-                                    @endforelse
+                                    <span class="help-block">При выборе товара он будет выбран по умолчанию при покеупке товара</span>
                                 </div>
-                            @endforeach
-                        </div>
-                        <div class="addComplectationCategoryForm">
-                            <div class="form-group">
-                                <lable>Добавить категорию к комплекту</lable>
-                                <span class="help-block">
+                                @forelse($complectationCategory->products->items() as $product)
+                                    <div
+                                            class="complectationProduct {{ $complectationCategory->getSelectedClass($product->product_id)  }}"
+                                            data-complection-id="{{ $complectation->id }}"
+                                            data-category-id="{{ $complectationCategory->category_id }}"
+                                            data-product-id="{{ $product->product_id }}"
+                                    >
+                                        <img src="{{ getImagePath($product->image) }}" alt="">
+                                    </div>
+                                @empty
+                                    <span class="help-block">Товарова нет!</span>
+                                @endforelse
+                            </div>
+                        @endforeach
+                    </div>
+                    <div class="addComplectationCategoryForm">
+                        <div class="form-group">
+                            <lable>Добавить категорию к комплекту</lable>
+                            <span class="help-block">
                                     При добавление нескольких категорий к комплекту при покупке товара у покупателя
                                     будет возможность выбора комплектоного товара их нескольких категорий
                                 </span>
-                                <select
-                                        name="main_category_id"
-                                        class="form-control complectationCategorySelect"
-                                        data-complection-id="{{ $complectation->id }}"
-                                >
-                                    @foreach($categories as $category)
-                                        <option value="{{ $category->category_id }}">{{ $category->description->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <button
-                                        class="btn btn-success storeComplectionCategory"
-                                        data-complection-id="{{ $complectation->id }}"
-                                >
-                                    Добавить
-                                </button>
-                            </div>
+                            <select
+                                    name="main_category_id"
+                                    class="form-control complectationCategorySelect"
+                                    data-complection-id="{{ $complectation->id }}"
+                            >
+                                @foreach($categories as $category)
+                                    <option value="{{ $category->category_id }}">{{ $category->description->name }}</option>
+                                @endforeach
+                            </select>
                         </div>
-                    </blockquote>
+                        <div class="form-group">
+                            <button
+                                    class="btn btn-success storeComplectionCategory"
+                                    data-complection-id="{{ $complectation->id }}"
+                            >
+                                Добавить
+                            </button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        @endforeach
+                <?php
+                $active = false;
+                ?>
+            @endforeach
+        </div>
     @endif
+    <hr>
     <h4>Добавить комплект</h4>
     <span class="help-block">
         При добавления комплекта к товару он будет продаваться в комплекте с выбранным покупателем товара,

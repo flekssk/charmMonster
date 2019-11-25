027a3bba157c33dcee0dda3084a65c984772032f
Aleksey Osipov <osipov@action-media.ru>
2019-11-26 00:39:48 +0300

    revert Product.js

---
+++ public/js/main/Product.js
@@ -0,0 +1,93 @@
+class Product extends Service {
+    constructor(options = {}) {
+        super(options);
+
+        this.rebindEvents();
+    }
+
+    rebindEvents() {
+        let object = this;
+
+        $('.imagesList .image').unbind();
+        $('.imagesList .image').click(function () {
+            $(".mainImage").trigger('zoom.destroy');
+            let clickedImage = $(this).find('img').attr('src');
+            $('.mainImage').find('img').attr('src', clickedImage);
+        });
+
+        $('.complectationProduct').unbind();
+        $('.complectationProduct').click(function () {
+            $('.complectationProduct')
+                .filter('[data-category-id="' + $(this).data('category-id') + '"]')
+                .removeClass('selected');
+
+            $('.selectedProductName')
+                .filter('[data-category-id="' + $(this).data('category-id') + '"]')
+                .find('.name')
+                .html($(this).data('original-title'));
+
+            $(this).addClass('selected');
+
+            object.repriceComplection();
+            object.changeProductComplection($(this).data('main-product'));
+        });
+
+        $('.complectionCategorySelect').unbind();
+        $('.complectionCategorySelect').change(function () {
+            let selectedCategory = $('.complectionCategorySelect').find('option:selected').val(),
+                productId = $(this).data('product-id');
+
+            $('.complectionCategoryProducts').removeClass('selected');
+            $('.complectionCategoryProducts').filter('[data-category-id="' + selectedCategory + '"]').addClass('selected');
+
+            $(document).ready(function () {
+                object.repriceComplection();
+                object.changeProductComplection(productId);
+            });
+        });
+
+        $('[data-toggle="tooltip"]').tooltip();
+    }
+
+    getSelectedComplection() {
+        let products = {};
+
+        $.each($('.complectionCategoryProducts.selected'), function (index, category) {
+            products[index] = $(category).find('.complectationProduct.selected').data('product-id');
+        });
+        return products;
+    }
+
+    repriceComplection() {
+        let baseProductPrice = Number.parseInt($('.mainProductPrice').data('base-price')),
+            totalPrice = 0;
+
+        $.each(this.getSelectedComplection(), function (index, product) {
+            totalPrice += Number.parseInt($('.complectationProduct[data-product-id="' + product + '"]').data('price'));
+        });
+
+        totalPrice += baseProductPrice;
+        $('.mainProductPrice').html(totalPrice);
+    }
+
+    changeProductComplection(productId) {
+        let complection = [];
+
+        $.each(this.getSelectedComplection(), function (index, product) {
+            complection.push($(product).data('product-id'));
+        });
+
+
+        $('.addToCart').filter('[data-product-id="' + productId + '"]').attr('data-complection', JSON.stringify(complection));
+    }
+
+    getSelectedComplectionProductsId() {
+        let complection = [];
+
+        $.each(this.getSelectedComplection(), function (index, product) {
+            complection.push($(product).data('product-id'));
+        });
+
+        return complection;
+    }
+}

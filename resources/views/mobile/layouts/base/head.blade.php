54a0274f5753a8a04d1992c58c94c2aa74b7a516
Aleksey Osipov <osipov@action-media.ru>
2019-12-03 22:01:20 +0300

    version

--- resources/views/mobile/layouts/base/head.blade.php
+++ resources/views/mobile/layouts/base/head.blade.php
@@ -1,12 +1,15 @@
+<?php
+$version = '1.1';
+?>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link rel="stylesheet" href="{{ asset('css/mobile/main.css') }}">
+<link rel="stylesheet" href="{{ asset('css/mobile/main.css?v=' . $version) }}">
 <link rel="stylesheet" href="{{ asset('font-awesome-4.7.0/css/font-awesome.min.css') }}">
-<link rel="stylesheet" href="{{ asset('css/mobile/order.css') }}">
-<link rel="stylesheet" href="{{ asset('css/mobile/product.css') }}">
+<link rel="stylesheet" href="{{ asset('css/mobile/order.css?v=' . $version) }}">
+<link rel="stylesheet" href="{{ asset('css/mobile/product.css?v=' . $version) }}">
 <link rel="stylesheet" href="{{ asset('lightbox/css/lightgallery.css') }}">
 <link href="https://cdn.jsdelivr.net/npm/suggestions-jquery@19.7.1/dist/css/suggestions.min.css" rel="stylesheet" />
 
